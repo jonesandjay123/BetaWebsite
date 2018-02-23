f229d125d532fdf8e1a1312a24542b7b625a739e
020518 <020518@CHQ-020518-NB2>
2018-02-23 10:01:48 +0800

    加入專案檔案。

---
+++ BetaWebsite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BetaWebsite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ BetaWebsite/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BetaWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "關於本公司...";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "歡迎透過email與我們聯繫，或是追蹤我們的Facebook粉絲團。";
+
+            return View();
+        }
+
+        public ActionResult TestPage1()
+        {
+            ViewBag.Message = "message測試的頁面";
+
+            return View();
+        }
+    }
+}
